aae7de04242c2528b9e99d654894b1328cacc992
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-08 15:54:29 -0300

    Add files via upload

---
+++ Exercicio3_Lista3.ConsoleApp/Exercicio3_Lista3.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio3_Lista3.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int zero = 0;
+            int soma = 0;
+
+            for (int i = 0; i < 500; i++)
+            {
+                zero = zero + 1;
+                if (zero % 2 != 0 && zero % 3 == 0)
+                {
+                    soma = soma + zero;
+                }            
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
