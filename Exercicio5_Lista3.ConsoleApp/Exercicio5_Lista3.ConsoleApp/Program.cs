58051b3b3152593bd983238c467484590a4d8c65
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-08 15:55:52 -0300

    Add files via upload

---
+++ Exercicio5_Lista3.ConsoleApp/Exercicio5_Lista3.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio5_Lista3.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+          
+            int fatorial = 1;         
+            for (int i = 0; i < A; i++)
+            {
+                fatorial = fatorial * (A - i);
+            }
+            Console.WriteLine("O fatorial de " + A  + " é: " + fatorial);
+        }
+    }
+}
