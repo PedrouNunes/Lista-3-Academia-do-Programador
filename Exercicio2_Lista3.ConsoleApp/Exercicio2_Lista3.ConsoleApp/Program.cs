66c1ce871bef32674e6acfadd12cccbe961a50ad
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-08 15:39:15 -0300

    Add files via upload

---
+++ Exercicio2_Lista3.ConsoleApp/Exercicio2_Lista3.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio2_Lista3.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cem = 99;
+
+            for (int i = 0; i < 50; i++)
+            {
+                cem = cem + 2;
+                Console.WriteLine(cem);
+            }
+            
+        }
+    }
+}
