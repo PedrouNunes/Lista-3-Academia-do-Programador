18d717376ce00735e4e1cfc30b47f68eca220c9f
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-08 15:55:10 -0300

    Add files via upload

---
+++ Exercicio4_Lista3.ConsoleApp/Exercicio4_Lista3.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio4_Lista3.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de A: ");
+            double A = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("A tabuada de " + A + " é: ");
+
+            double tabuada;
+            for (int i = 0; i < 11; i++)
+            {  
+                tabuada = A * i;
+                Console.WriteLine(A + " X " + i + " = " + tabuada);
+            }
+        }
+    }
+}
